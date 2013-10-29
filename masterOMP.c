8649d2a4f48cad3fb5269870b6a3712cf844c27f
Rodrigo Echeverria Correa <rodrigoecheverria@outlook.com>
2013-10-29 16:26:12 +0100

    correction on 6for

--- masterOMP.c
+++ masterOMP.c
@@ -89,9 +89,9 @@ void mult_mat ( double *const a, double *const b, double *restrict c, int N )
   for (i=0;i<N; i+=SM)
     for(j=0;j<N;j+=SM)
       for(k=0;k<N;k+=SM)
-        for(i2=0;i2<SM;++i2)
-          for(k2=0;k2<SM;++k2)
-            for(j2=0;j2<SM;++j2)
+        for(i2=0;i2<SM;i2++)
+          for(k2=0;k2<SM;k2++)
+            for(j2=0;j2<SM;j2++)
               c[i*N+j2] += a[i*N+k2]*b[k*N+j2];
 /*  double *T;
   T = (double *) malloc ( N*N*sizeof(double));

d32e8d579a742f4bd5ac589352f162f216d540fc
Rodrigo Echeverria Correa <rodrigoecheverria@outlook.com>
2013-10-29 16:55:04 +0100

    tiled correction

--- masterOMP.c
+++ masterOMP.c
@@ -90,9 +90,9 @@ void mult_mat ( double *const a, double *const b, double *restrict c, int N )
     for(j=0;j<N;j+=SM)
       for(k=0;k<N;k+=SM)
         for(i2=0;i2<SM;i2++)
-          for(k2=0;k2<SM;k2++)
-            for(j2=0;j2<SM;j2++)
-              c[i*N+j2] += a[i*N+k2]*b[k*N+j2];
+          for(j2=0;j2<SM;j2++)
+            for(k2=0;k2<SM;k2++)
+              c[i*N+j] += a[i*N+k2]*b[k*N+j2];
 /*  double *T;
   T = (double *) malloc ( N*N*sizeof(double));
   zero_mat(T,N);

247a665a7ab2001a6ce5ea4d2b38542998cb898b
Rodrigo Echeverria Correa <rodrigoecheverria@outlook.com>
2013-10-30 12:38:44 +0100

    Throw away the impossible

--- masterOMP.c
+++ masterOMP.c
@@ -102,12 +102,23 @@ void mult_mat ( double *const a, double *const b, double *restrict c, int N )
           {
             __m128d m1d = _mm_load_sd(&a2[k2]);
             m1d=_mm_unpacklo_pd (m1d,m1d);
+            
             for(j2=0;j2<SM;j2+=2)
+            {
+              __m128d m2,r2;
+              if ((i*N + j+ j2) < (N*N))
+              {
+                m2 = _mm_load_pd(&b2[j2]);
+                r2 = _mm_load_pd(&c2[j2]);
+                _mm_store_pd (&c2[j2],_mm_add_pd (_mm_mul_pd(m2,m1d),r2));
+              }
+            }
+            /*for(j2=0;j2<SM;j2+=2)
             {
                 __m128d m2 = _mm_load_pd(&b2[j2]);
                 __m128d r2 = _mm_load_pd(&c2[j2]);
                 _mm_store_pd (&c2[j2],_mm_add_pd (_mm_mul_pd(m2,m1d),r2));
-            }
+            }*/
           }
         }
 printf ("START LAST ITERATION--------------------------------------\n");

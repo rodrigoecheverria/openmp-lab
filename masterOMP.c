4cd488feb9e459cb16a5ab8b77a1935158f2ebb8
Rodrigo Echeverria Correa <rodrigoecheverria@outlook.com>
2013-10-30 11:35:20 +0100

    The point is to go fast

--- masterOMP.c
+++ masterOMP.c
@@ -90,41 +90,52 @@ void mult_mat ( double *const a, double *const b, double *restrict c, int N )
   double *restrict a2; 
   double *restrict b2;
   double *restrict c2;
-#pragma omp parallel for
-  for (i=0;i<N; i+=SM){
+#pragma omp parallel 
+{
+#pragma omp for
+  for (i=0;i<(N-SM); i+=SM)
     for(j=0;j<N;j+=SM)
       for(k=0;k<N;k+=SM)
-      {
         for(i2=0,c2=&c[i*N+j],a2=&a[i*N+k];i2<SM;++i2,c2+=N,a2+=N)
         {
-            //printf("first inner\n");
           _mm_prefetch (&a2[8],_MM_HINT_NTA);
-          
           for(k2=0,b2=&b[k*N+j];k2<SM;++k2,b2+=N)
           {
-            //if(i==1992) printf("second inner\n");
             __m128d m1d = _mm_load_sd(&a2[k2]);
             m1d=_mm_unpacklo_pd (m1d,m1d);
             for(j2=0;j2<SM;j2+=2)
             {
-                /*printf("third inner\n");
-                printf("First pointer: %p\n", &b2[j2]);
-                printf("Second pointer: %p\n", &b2[j2+1]);
-                printf("Third pointer: %p\n", &b2[j2+2]);*/
-                __m128d m2;
-                __m128d r2;
-              if((i*N+j+j2) < (N * N))
-              {
-                m2 = _mm_load_pd(&b2[j2]);
-                r2 = _mm_load_pd(&c2[j2]);
+                __m128d m2 = _mm_load_pd(&b2[j2]);
+                __m128d r2 = _mm_load_pd(&c2[j2]);
                 _mm_store_pd (&c2[j2],_mm_add_pd (_mm_mul_pd(m2,m1d),r2));
-              }
-              //c2[j2] += a2[k2]*b2[j2];
             }
           }
         }
+//LAST ITERATION UNROLLED (i = N-SM )
+#pragma omp for
+for(j=0;j<N;j+=SM)
+  for(k=0;k<N;k+=SM)
+    for(i2=0,c2=&c[i*N+j],a2=&a[i*N+k];i2<SM;++i2,c2+=N,a2+=N)
+    {
+      _mm_prefetch (&a2[8],_MM_HINT_NTA);
+      for(k2=0,b2=&b[k*N+j];k2<SM;++k2,b2+=N)
+      {
+        __m128d m1d = _mm_load_sd(&a2[k2]);
+        m1d=_mm_unpacklo_pd (m1d,m1d);
+        for(j2=0;j2<SM;j2+=2)
+        {
+          __m128d m2,r2;
+          if ((i*N + j+ j2) < (N*N))
+          {
+            m2 = _mm_load_pd(&b2[j2]);
+            r2 = _mm_load_pd(&c2[j2]);
+            _mm_store_pd (&c2[j2],_mm_add_pd (_mm_mul_pd(m2,m1d),r2));
+          }
+        }
+        
       }
-  }
+    }
+}
 /*  double *T;
   T = (double *) malloc ( N*N*sizeof(double));
   zero_mat(T,N);
